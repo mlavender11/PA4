0b1a136a5d5660c4fcf11bb50fb28813aabacf31
Michael Lavender <michaelzlavender@gmail.com>
2026-04-18 20:08:41 -0400

    added input error checking

--- GameCommand.cpp
+++ GameCommand.cpp
@@ -1,5 +1,6 @@
 #include "GameCommand.h"
 #include "Model.h"
+#include "Invalid_Input.h"
 
 void DoMoveCommand(Model &model, int mage_id, Point2D p1)
 {
@@ -51,7 +52,7 @@ void DoRecoverInSpireCommand(Model &model, int mage_id, unsigned int crystals_ne
     mage->StartRecoveringMana(crystals_needed);
 }
 
-void DoAdvanceCommand(Model& model, View& view)
+void DoAdvanceCommand(Model &model, View &view)
 {
     cout << "Advancing one tick" << endl;
     model.Update();
@@ -59,52 +60,79 @@ void DoAdvanceCommand(Model& model, View& view)
     model.Display(view);
 }
 
-void DoRunCommand(Model& model, View& view)
+void DoRunCommand(Model &model, View &view)
 {
     cout << "Advanding to next event" << endl;
     for (int steps = 0; steps < 5; steps++)
     {
-        if(model.Update()) {
+        if (model.Update())
+        {
             break;
         }
-
     }
     model.ShowStatus();
     model.Display(view);
 }
 
-
-void getInput(Model& model, View& view) {
-    cout << endl << "*********" << endl;
+void getInput(Model &model, View &view)
+{
+    cout << endl
+         << "*********" << endl;
     char code;
     int in1, in2, in3;
 
-    cout << "Enter a command: "; //check if this is correct
+    cout << "Enter a command: ";
     cin >> code;
 
-    switch (code) {
+    try
+    {
+        switch (code)
+        {
         case 'm':
             cin >> in1 >> in2 >> in3;
+            if (!model.GetMagePtr(in1))
+                throw Invalid_Input("No mage with id: " + to_string(in1));
+            if (in2 < 0 || in2 > view.GetMaxSize() || in3 < 0 || in3 > view.GetMaxSize())
+                throw Invalid_Input("Coordinates (" + to_string(in2) + ", " + to_string(in3) + ") are out of bounds");
             DoMoveCommand(model, in1, Point2D(in2, in3));
             break;
+
         case 's':
             cin >> in1 >> in2;
+            if (!model.GetMagePtr(in1))
+                throw Invalid_Input("No mage with id: " + to_string(in1));
+            if (!model.GetManaSpirePtr(in2))
+                throw Invalid_Input("No spire with id: " + to_string(in2));
             DoMoveToSpireCommand(model, in1, in2);
             break;
         case 'd':
             cin >> in1 >> in2;
+            if (!model.GetMagePtr(in1))
+                throw Invalid_Input("No mage with id: " + to_string(in1));
+            if (!model.GetDemonHideoutPtr(in2))
+                throw Invalid_Input("No hideout with id: " + to_string(in2));
             DoMoveToHideoutCommand(model, in1, in2);
             break;
         case 'h':
             cin >> in1;
+            if (!model.GetMagePtr(in1))
+                throw Invalid_Input("No mage with id: " + to_string(in1));
             DoStopCommand(model, in1);
             break;
         case 'c':
             cin >> in1 >> in2;
+            if (!model.GetMagePtr(in1))
+                throw Invalid_Input("No mage with id: " + to_string(in1));
+            if (model.GetMagePtr(in1)->GetState() != AT_SPIRE)
+                throw Invalid_Input("Mage " + to_string(in1) + " is not at a ManaSpire");
             DoRecoverInSpireCommand(model, in1, in2);
             break;
         case 'b':
             cin >> in1 >> in2;
+            if (!model.GetMagePtr(in1))
+                throw Invalid_Input("No mage with id: " + to_string(in1));
+            if (model.GetMagePtr(in1)->GetState() != IN_HIDEOUT)
+                throw Invalid_Input("Mage " + to_string(in1) + " is not at a DemonHideout");
             DoBattleCommand(model, in1, in2);
             break;
         case 'a':
@@ -117,7 +145,16 @@ void getInput(Model& model, View& view) {
             cout << "Quitting" << endl;
             exit(0);
             break;
+
+        default:
+            throw Invalid_Input("Unkown command entered");
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    catch (Invalid_Input &e)
+    {
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Error: " << e.msg_ptr << endl;
     }
 }
-
-
