95f772d214f722e9cea52606926494fae0fca5fa
Michael Lavender <michaelzlavender@gmail.com>
2026-04-26 23:16:35 -0400

    added save to Model

--- Model.h
+++ Model.h
@@ -5,9 +5,6 @@
 // that it is no longer updated. For debugging and demonstration purposes, output a message like
 // Dead object removed.
 
-
-
-
 #ifndef MODEL_H
 #define MODEL_H
 #include "View.h"
@@ -25,13 +22,12 @@ class Model
 private:
     int time;
 
-    list<GameObject*> object_ptrs;
-    list<GameObject*> active_ptrs;
-    list<Mage*> mage_ptrs;
-    list<ManaSpire*> spire_ptrs;
-    list<DemonHideout*> hideout_ptrs;
-    list<RoamingDemon*> roaming_ptrs;
-
+    list<GameObject *> object_ptrs;
+    list<GameObject *> active_ptrs;
+    list<Mage *> mage_ptrs;
+    list<ManaSpire *> spire_ptrs;
+    list<DemonHideout *> hideout_ptrs;
+    list<RoamingDemon *> roaming_ptrs;
 
 public:
     Model();
@@ -47,6 +43,9 @@ public:
     void ShowStatus();
 
     void NewCommand(char type, int id, Point2D loc);
+
+    void save(ofstream &file) const;
+    void restore(ofstream &file);
 };
 
 #endif
